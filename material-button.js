ddd53b58b082b0909f8f9fa4fc81513c893e6a92
Akshay Nair <phenax5@gmail.com>
2016-10-23 22:34:09 +0530

    Fixed the ripple size bug

--- material-button.js
+++ material-button.js
@@ -42,7 +42,15 @@ class MaterialBtn extends HTMLElement {
 		this._initializeComponent();
 	}
 
+	_setUpComponent() {
+
+		this._color= this.dataset.color || this.DEFAULT_COLOR;
+
+		this.text= this.textContent || '';
+	}
+
 	_initializeComponent() {
+
 		this._$textEl.textContent= this.text;
 
 		this._$button.style.backgroundColor= this.color;
@@ -51,6 +59,7 @@ class MaterialBtn extends HTMLElement {
 	}
 
 	_buttonClickHandler(e) {
+
 		this.triggerRipple({
 			x: e.pageX,
 			y: e.pageY
@@ -67,17 +76,16 @@ class MaterialBtn extends HTMLElement {
 		const dimens= this._$button.getBoundingClientRect();
 
 		this._currentRipple= { scale: 0, opacity: 1 };
-		this._targetRipple= {
-			scale: Math.sqrt(
-				Math.pow(dimens.width, 2) + 
-				Math.pow(dimens.height, 2)
-			),
-			opacity: 0,
-		};
-
 		this._clickPos= {
 			x: clickPos.x - dimens.left,
 			y: clickPos.y - dimens.top
+		};
+		this._targetRipple= {
+			scale: 2* Math.sqrt(
+				Math.pow((this._clickPos.x > dimens.width/2)? this._clickPos.x: dimens.width - this._clickPos.x, 2) + 
+				Math.pow((this._clickPos.y > dimens.height/2)? this._clickPos.y: dimens.height - this._clickPos.y, 2)
+			),
+			opacity: 0,
 		};
 
 		window.requestAnimationFrame(this._renderLoop);
@@ -85,11 +93,14 @@ class MaterialBtn extends HTMLElement {
 
 	_renderLoop() {
 
-		this._$ripple.style.transform= `translate(${this._clickPos.x}px, ${this._clickPos.y}px) scale(${this._currentRipple.scale})`;
+		this._$ripple.style.transform= `
+			translate(${this._clickPos.x}px, ${this._clickPos.y}px)
+			scale(${this._currentRipple.scale})
+		`;
 		this._$ripple.style.opacity= this._currentRipple.opacity;
 
-		this._currentRipple.scale += (this._targetRipple.scale - this._currentRipple.scale)/10;
-		this._currentRipple.opacity += (this._targetRipple.opacity - this._currentRipple.opacity)/10;
+		this._currentRipple.scale +=   (this._targetRipple.scale - this._currentRipple.scale)/10;
+		this._currentRipple.opacity += (this._targetRipple.opacity - this._currentRipple.opacity)/20;
 
 		if( this._currentRipple.opacity <= 0.01 )
 			this._isAnimating= false;
@@ -98,13 +109,6 @@ class MaterialBtn extends HTMLElement {
 			window.requestAnimationFrame(this._renderLoop);
 		else
 			this._$ripple.style.opacity= 0;
-	}
-
-	_setUpComponent() {
-
-		this._color= this.dataset.color || this.DEFAULT_COLOR;
-		
-		this.text= this.textContent || '';
 	}
 
 	disconnectedCallback() {
